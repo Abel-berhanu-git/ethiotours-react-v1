880a3d37789a599324796201103403fac4945177
AbelBerhanu <abelberhanu2018@gmail.com>
2026-02-09 11:31:10 +0300

    adding navbar toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,52 @@
+import { useState } from 'react';
 import { pageLinks, socialLinks } from '../data';
 import logo from '../images/logo.svg';
 import PageLink from './PageLink';
-import SocialLinks from './SocialLink'
+import SocialLinks from './SocialLink';
+import { useRef } from 'react';
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const linkContainer = useRef(null)
+  const links = useRef(null)
+  // console.log(links.current.getBoundingClientRect().height);
+
+  const linkStyle = {
+    height: showNavbar
+      ? `${links.current.getBoundingClientRect().height}px`
+      : '0px',
+  };
   return (
     <nav className='navbar'>
       <div className='nav-center'>
         {/* nav-header */}
         <div className='nav-header'>
           <img src={logo} className='nav-logo' alt='nav-logo' />
-          <button type='button' className='nav-toggle' id='nav-toggle'>
+          <button type='button' className='nav-toggle' id='nav-toggle' onClick={()=> setShowNavbar(!showNavbar)}>
             <i className='fa-solid fa-bars'></i>
           </button>
         </div>
         {/* end of nav-header */}
 
         {/* nav links */}
-        <ul className='nav-links' id='nav-links'>
-          {pageLinks.map((link) => {
-            const { id, href, text } = link;
-            return (
-              <PageLink
-                key={id}
-                href={href}
-                text={text}
-                itemClass={'nav-link'}
-              />
-            );
-          })}
-        </ul>
+        <div className='nav-container' ref={linkContainer} style={linkStyle}>
+          <ul className='nav-links' id='nav-links' ref={links}>
+            {pageLinks.map((link) => {
+              const { id, href, text } = link;
+              return (
+                <PageLink
+                  key={id}
+                  href={href}
+                  text={text}
+                  itemClass={'nav-link'}
+                  showNavbar={showNavbar}
+                  setShowNavbar={setShowNavbar}
+                />
+              );
+            })}
+          </ul>
+        </div>
+
         {/* end of nav links */}
 
         {/* nav-icons */}
--- src/components/PageLink.jsx
+++ src/components/PageLink.jsx
@@ -1,9 +1,9 @@
 // use this for later if you want more abstraction and  more control over the links
 
-const PageLink = ({ href, text,itemClass }) => {
+const PageLink = ({ href, text,itemClass,showNavbar,setShowNavbar }) => {
   return (
-    <li>
-      <a href={href} className={itemClass}>
+    <li onClick={()=> setShowNavbar(!showNavbar)}>
+      <a href={href} className={itemClass} >
         {text}
       </a>
     </li>
